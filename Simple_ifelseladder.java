6e9225db0bb11d8b5d44a67c939d92e20839d969
Tushar Jetagi <112852721+TusharJetagi@users.noreply.github.com>
2023-03-17 21:20:24 +0530

    Add files via upload

---
+++ Simple_ifelseladder.java
@@ -0,0 +1,22 @@
+class Simple_ifelseladder
+{
+public static void main(String args[])
+{
+
+String name = "Tushar";
+
+if(name == "Virat")
+{
+System.out.println("Name is virat");
+}
+else if (name == "Rohit")
+{
+System.out.println("Name is rohit");
+}
+else if (name == "Maxwell")
+{
+System.out.println("Name is maxwell");
+}
+else{
+System.out.println(name);
+}}}
---
+++ logical_op.java
@@ -0,0 +1,11 @@
+class logical_op{
+public static void main(Srting agrs[]){
+
+int a = 20;
+int b = 5;
+int c = 15;
+
+System.out.println(a>b && a<c); 
+System.out.println(a<c || b>a); 
+}
+}
