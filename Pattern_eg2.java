4b4992a55395fe1b97e7e04423cd365555ec3b2a
Tushar Jetagi <112852721+TusharJetagi@users.noreply.github.com>
2023-03-21 20:13:44 +0530

    Add files via upload

---
+++ Pattern_eg2.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class Pattern_eg2 {
+	public static void main(String[] args) {
+		int row = 5;
+		for(int i=5; i>=1; i--) { // It will print row
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+}
+}
+	}
