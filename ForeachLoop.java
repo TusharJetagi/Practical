fb6c018679d94b712a5fc312f43437ae9366b73a
Tushar Jetagi <112852721+TusharJetagi@users.noreply.github.com>
2023-03-21 09:04:38 +0530

    Add files via upload

---
+++ ForeachLoop.java
@@ -0,0 +1,9 @@
+class ForeachLoop{
+public static void main(String args[]){
+
+String[] names = {"Riya","Rohit","Meena","Amit","Shweta"};
+System.out.println("Printing the contents of the student names: \n");
+
+for(String result:names){
+System.out.println(result);
+}}}
