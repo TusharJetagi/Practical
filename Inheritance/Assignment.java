1f2d0a6a928d254bc459d2d8ee6b599e58df03df
Tushar Jetagi <112852721+TusharJetagi@users.noreply.github.com>
2023-04-03 22:47:48 +0530

    Add files via upload

---
+++ Inheritance/Assignment.java
@@ -0,0 +1,21 @@
+package Inheritance;
+class Arithmetic{
+	int result;
+	void add(int num_1,int num_2) 
+	{
+		result = num_1+num_2;
+	}
+}
+class adder extends Arithmetic{
+	void show() {
+		System.out.println("Result is: "+result);
+	}
+}
+public class Assignment {
+	public static void main(String[] args) {
+		adder a = new adder();
+		a.add(10, 20);
+		a.show();
+	}
+
+}
---
+++ Inheritance/Example.java
@@ -0,0 +1,27 @@
+package Inheritance;
+class A {
+void show() {
+	System.out.println("Class A ");
+}
+}
+class B extends A{
+	void print() {
+		System.out.println("Class B");
+	}
+}
+class C extends B{
+	void display() {
+		System.out.println("Class C");
+	}
+}
+public class Example {
+	public static void main(String[] args) {
+		C obj = new C();
+		obj.show();
+		obj.print();
+		obj.display();
+		
+		
+	}
+
+}
