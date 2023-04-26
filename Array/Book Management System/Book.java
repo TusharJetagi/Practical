055722f2fcf1b494edf06f988a01d5dc3a3338c9
Tushar Jetagi <112852721+TusharJetagi@users.noreply.github.com>
2023-04-26 22:03:37 +0530

    Add files via upload

---
+++ Array/Book Management System/Book.java
@@ -0,0 +1,47 @@
+package Array;
+
+public class Book 
+{
+	private int bookId;
+	private String bookName;
+	private double bookPrice;
+	
+	public int getBookId() 
+	{
+		return bookId;
+	}
+	public void setBookId(int bookId) 
+	{
+		this.bookId = bookId;
+	}
+	public String getBookName() 
+	{
+		return bookName;
+	}
+	public void setBookName(String bookName) 
+	{
+		this.bookName = bookName;
+	}
+	public double getBookPrice() 
+	{
+		return bookPrice;
+	}
+	public void setBookPrice(double bookPrice) 
+	{
+		this.bookPrice = bookPrice;
+	}
+	
+	// no args constructor
+	public Book() 
+	{
+		super();
+	}
+	public Book(int bookId, String bookName, double bookPrice) 
+	{
+		super();
+		this.bookId = bookId;
+		this.bookName = bookName;
+		this.bookPrice = bookPrice;
+	}
+
+}
